ef0c8d53e5828235ffee1275488b8e80846c25c4
Allen <54394182+PrintName@users.noreply.github.com>
2019-11-02 15:24:16 -0400

    9.2 Complete

---
+++ Snacktacular-Starter-Project-master/Snacktacular/Spots.swift
@@ -0,0 +1,13 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Allen Li on 11/2/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots {
+    var spotArray = [Spot]()
+}
