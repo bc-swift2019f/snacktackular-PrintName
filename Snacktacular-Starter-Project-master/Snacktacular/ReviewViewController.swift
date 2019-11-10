48f1a5dbb955f0ff86fd0958bdeaea5dfe0a5a4b
Allen <54394182+PrintName@users.noreply.github.com>
2019-11-09 20:08:45 -0500

    9.13 Complete

--- Snacktacular-Starter-Project-master/Snacktacular/ReviewViewController.swift
+++ Snacktacular-Starter-Project-master/Snacktacular/ReviewViewController.swift
@@ -20,6 +20,17 @@ class ReviewViewController: UITableViewController {
     @IBOutlet weak var saveBarButton: UIBarButtonItem!
     @IBOutlet weak var deleteButton: UIButton!
     @IBOutlet weak var buttonsBackgroundView: UIView!
+    @IBOutlet var starButtonCollection: [UIButton]!
+    
+    var rating = 0 {
+        didSet {
+            for starButton in starButtonCollection {
+                let image = UIImage(named: ( starButton.tag < rating ? "star-filled" : "star-empty") )
+                starButton.setImage(image, for: .normal)
+            }
+            print(">>> New Rating: \(rating)")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +46,10 @@ class ReviewViewController: UITableViewController {
         } else {
             navigationController?.popViewController(animated: true)
         }
+    }
+    
+    @IBAction func starButtonPressed(_ sender: UIButton) {
+        rating = sender.tag + 1
     }
     
     @IBAction func reviewTitleChanged(_ sender: UITextField) {
