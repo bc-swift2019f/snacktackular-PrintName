f9089234aa2ee0d5a9b54e48e114176e7cdde7c1
Allen <54394182+PrintName@users.noreply.github.com>
2019-11-09 23:12:17 -0500

    9.16 Complete

---
+++ Snacktacular-Starter-Project-master/Snacktacular/Photo.swift
@@ -0,0 +1,29 @@
+//
+//  Photo.swift
+//  Snacktacular
+//
+//  Created by Allen Li on 11/9/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Photo {
+    var image: UIImage
+    var description: String
+    var postedBy: String
+    var date: Date
+    var documentUUID: String // Universal Unique IDentifier
+    
+    init(image: UIImage, description: String, postedBy: String, date: Date, documentUUID: String) {
+        self.image = image
+        self.description = description
+        self.postedBy = postedBy
+        self.date = date
+        self.documentUUID = documentUUID
+    }
+    
+    
+    
+}
--- Snacktacular-Starter-Project-master/Snacktacular/SpotDetailViewController.swift
+++ Snacktacular-Starter-Project-master/Snacktacular/SpotDetailViewController.swift
@@ -24,6 +24,7 @@ class SpotDetailViewController: UIViewController {
     
     var spot: Spot!
     var reviews: Reviews!
+    var photos: Photos!
     var regionDistance: CLLocationDistance = 750
     var locationManager: CLLocationManager!
     var currentLocation: CLLocation!
@@ -38,6 +39,8 @@ class SpotDetailViewController: UIViewController {
         // mapView.delegate = self
         tableView.delegate = self
         tableView.dataSource = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
         if spot == nil {
             spot = Spot()
@@ -54,6 +57,7 @@ class SpotDetailViewController: UIViewController {
             navigationController?.setToolbarHidden(true, animated: true)
         }
         reviews = Reviews()
+        photos = Photos()
         
         let region = MKCoordinateRegion(center: spot.coordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
         mapView.setRegion(region, animated: true)
@@ -90,17 +94,6 @@ class SpotDetailViewController: UIViewController {
         }
     }
     
-    @IBAction func textFieldEditingChanged(_ sender: UITextField) {
-        saveBarButton.isEnabled = !(nameField.text == "")
-    }
-    
-    @IBAction func textFieldReturnPressed(_ sender: UITextField) {
-        sender.resignFirstResponder()
-        spot.name = nameField.text!
-        spot.address = addressField.text!
-        updateUserInterface()
-    }
-    
     func showAlert(title: String, message: String) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
@@ -128,8 +121,20 @@ class SpotDetailViewController: UIViewController {
         mapView.addAnnotation(spot)
         mapView.setCenter(spot.coordinate, animated: true)
     }
-
+    
+    @IBAction func textFieldEditingChanged(_ sender: UITextField) {
+        saveBarButton.isEnabled = !(nameField.text == "")
+    }
+    
+    @IBAction func textFieldReturnPressed(_ sender: UITextField) {
+        sender.resignFirstResponder()
+        spot.name = nameField.text!
+        spot.address = addressField.text!
+        updateUserInterface()
+    }
+    
     @IBAction func photoButtonPressed(_ sender: UIButton) {
+        cameraOrLibraryAlert()
     }
     
     @IBAction func reviewButtonPressed(_ sender: UIButton) {
@@ -146,6 +151,17 @@ class SpotDetailViewController: UIViewController {
                 print("*** ERROR: Couldn't leave this view controller because data wasn't saved.")
             }
         }
+    }
+    
+    func cameraOrLibraryAlert() {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: nil)
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cameraAction)
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func lookupPlacePressed(_ sender: Any) {
@@ -259,6 +275,19 @@ extension SpotDetailViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ReviewCell", for: indexPath) as! SpotReviewsTableViewCell
         cell.review = reviews.reviewArray[indexPath.row]
+        return cell
+    }
+    
+}
+
+extension SpotDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.photoArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! SpotPhotosCollectionViewCell
+        cell.photo = photos.photoArray[indexPath.row]
         return cell
     }
     
